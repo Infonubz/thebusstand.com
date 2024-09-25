637b86be925586810c519a2aa3d44fbcea6d143e
erp4 <erp4@akr.com>
2024-09-25 06:33:24 +0530

    test

--- src/setupTests.js
+++ src/setupTests.js
@@ -7,3 +7,14 @@ import '@testing-library/jest-dom';
 // Mock TextEncoder and TextDecoder for Jest environment
 global.TextEncoder = require('util').TextEncoder;
 global.TextDecoder = require('util').TextDecoder;
+const { createCanvas } = require('canvas');
+global.HTMLCanvasElement.prototype.getContext = function () {
+  return createCanvas().getContext('2d');
+};
+const crypto = require('crypto');
+
+Object.defineProperty(global, 'crypto', {
+  value: {
+    getRandomValues: (arr) => crypto.randomBytes(arr.length),
+  },
+});
