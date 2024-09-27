f3fed57fa887c6ba14f9f689f26e3df0dbef3606
erp4 <erp4@akr.com>
2024-09-27 06:57:49 +0530

    test

--- src/jest.config.js
+++ src/jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  testMatch: ['<rootDir>/src/**/*.{spec,test}.{js,jsx,ts,tsx}'],
+  "testMatch": ["<rootDir>/src/**/*.{spec,test}.{js,jsx,ts,tsx}"],
 
     testPathIgnorePatterns: ['/node_modules/'],
   };
